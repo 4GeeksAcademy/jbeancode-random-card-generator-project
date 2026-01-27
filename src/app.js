3eb1d0172a63a613dd255f12b5c97dbfd422fee5
JBeanCode <js02bean@gmail.com>
2026-01-27 01:59:32 +0000

    joshs-first-draft-card-generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,32 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  const cardNumbers = ["A","2","3","4",
+                      "5","6","7","8",
+                      "9","10","J","Q","K"
+  ];
+  const cardSuits = [{suit: "♦", color: "red"},{suit: "♥", color: "red"}, {suit: "♠", color: "black"}, {suit: "♣", color: "black"}];
+  //set the text.color equal to the key ("color")
+
+  function getRandomIndex(array) {
+    return Math.floor(Math.random() * array.length)
+}
+
+let createCardValues = () => {
+
+  const card = {}; //open object
+  card.value = cardNumbers[getRandomIndex(cardNumbers)];
+  let cardSuitSelect = cardSuits[getRandomIndex(cardSuits)]
+  card.color = cardSuitSelect.color
+  card.suit = cardSuitSelect.suit
+  return card
+}
+
+
   //write your code here
-  console.log("Hello Rigo from the console!");
+//♦ ♥ ♠ ♣
+
+  console.log(createCardValues());
 };
+
+
